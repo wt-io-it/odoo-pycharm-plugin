58e1a54ca613346d6c94f0f50a6959a21a0a09c5
bigbear3001 <bigbear.ap@gmail.com>
2019-10-01 17:38:53 +0200

    [IMP] enable resolving model names for CTRL click in keyword arguments
    and _name and _inherit

--- src/at/wtioit/intellij/plugins/odoo/OdooGoToDeclarationHandler.java
+++ src/at/wtioit/intellij/plugins/odoo/OdooGoToDeclarationHandler.java
@@ -8,9 +8,8 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.jetbrains.python.psi.PyCallExpression;
-import com.jetbrains.python.psi.PyPlainStringElement;
-import com.jetbrains.python.psi.PyStringElement;
+import com.jetbrains.python.psi.*;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class OdooGoToDeclarationHandler extends GotoDeclarationHandlerBase {
@@ -19,11 +18,35 @@ public class OdooGoToDeclarationHandler extends GotoDeclarationHandlerBase {
         if (psiElement.getParent().getParent().getParent() instanceof PyCallExpression) {
             String fieldType = ((PyCallExpression) psiElement.getParent().getParent().getParent()).getCallee().getText();
             if (OdooModel.ODOO_MODEL_NAME_FIELD_NAMES.contains(fieldType)) {
-                OdooModelService modelService = ServiceManager.getService(psiElement.getContainingFile().getProject(), OdooModelService.class);
-                TextRange range = ((PyStringElement) psiElement).getContentRange();
-                String value = psiElement.getText().substring(range.getStartOffset(), range.getEndOffset());
-                return modelService.getModel(value).getDefiningElement();
+                return getOdooModel(psiElement);
             }
+        } else if (psiElement.getParent().getParent() instanceof PyKeywordArgument
+                && psiElement.getParent().getParent().getParent().getParent() instanceof PyCallExpression) {
+            String fieldType = ((PyCallExpression) psiElement.getParent().getParent().getParent().getParent()).getCallee().getText();
+            if (OdooModel.ODOO_MODEL_NAME_FIELD_NAMES.contains(fieldType)) {
+                String keyword = ((PyKeywordArgument) psiElement.getParent().getParent()).getKeyword();
+                if (OdooModel.ODOO_MODEL_NAME_FIELD_KEYWORD_ARGUMENTS.contains(keyword)) {
+                    return getOdooModel(psiElement);
+                }
+            }
+        } else if (psiElement.getParent() instanceof PyStringLiteralExpression
+                && psiElement.getParent().getParent() instanceof PyAssignmentStatement) {
+            String variableName = psiElement.getParent().getParent().getFirstChild().getText();
+            if (OdooModel.ODOO_MODEL_NAME_VARIABLE_NAME.contains(variableName)) {
+                return getOdooModel(psiElement);
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    private PsiElement getOdooModel(@NotNull PsiElement psiElement) {
+        OdooModelService modelService = ServiceManager.getService(psiElement.getContainingFile().getProject(), OdooModelService.class);
+        TextRange range = ((PyStringElement) psiElement).getContentRange();
+        String value = psiElement.getText().substring(range.getStartOffset(), range.getEndOffset());
+        OdooModel model = modelService.getModel(value);
+        if (model != null) {
+            return model.getDefiningElement();
         }
         return null;
     }
