0593f38ba2bbd6ef2c5f655bcebbedfe0797cbfa
Andreas Perhab <bigbear.ap@gmx.at>
2019-10-12 13:37:15 +0200

    add test for model names list and model resolving for python class

--- src/test/java/at/wtioit/intellij/plugins/odoo/models/TestModelServiceImpl.java
+++ src/test/java/at/wtioit/intellij/plugins/odoo/models/TestModelServiceImpl.java
@@ -3,8 +3,13 @@ package at.wtioit.intellij.plugins.odoo.models;
 import at.wtioit.intellij.plugins.odoo.BaseOdooPluginTest;
 import at.wtioit.intellij.plugins.odoo.modules.OdooModule;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.python.psi.PyClass;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class TestModelServiceImpl extends BaseOdooPluginTest {
 
@@ -23,6 +28,26 @@ public class TestModelServiceImpl extends BaseOdooPluginTest {
         assertNotNull(inherited.getBaseModule().getDirectory());
         assertEquals("Base Model should be addon3", "PsiDirectory:/src/odoo/addons/addon3", inherited.getBaseModule().getDirectory().toString());
         assertContainsElements(inherited.getModules().stream().map(OdooModule::getName).collect(Collectors.toList()), "addon3", "addon1");
+    }
+
+    public void testModelNames() {
+        OdooModelService modelService = ServiceManager.getService(getProject(), OdooModelService.class);
+        assertContainsElements(StreamSupport.stream(modelService.getModelNames().spliterator(), false).collect(Collectors.toList()), "inherited", "existing");
+    }
+
+    public void testGetModelForElement() {
+        PsiFile psiFile = myFixture.addFileToProject("odoo/addons/addon1/models/model_for_element.py", "" +
+                "from odoo import models\n" +
+                "\n" +
+                "class ModelForElement(models.Model):" +
+                "   _name = 'model_for_element'");
+        forceRescan();
+        PsiElement pyClass = Arrays.stream(psiFile.getChildren()).filter(psiElement -> psiElement instanceof PyClass).findFirst().orElseThrow(AssertionError::new);
+
+        OdooModelService modelService = ServiceManager.getService(getProject(), OdooModelService.class);
+        OdooModel modelForElement = modelService.getModelForElement(pyClass);
+        assertNotNull(modelForElement);
+        assertEquals("model_for_element", modelForElement.getName());
     }
 
 }
