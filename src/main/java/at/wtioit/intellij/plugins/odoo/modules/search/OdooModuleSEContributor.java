86cc1759e5d91f13ba6e41a4d6451e8251e0e2f2
bigbear3001 <bigbear.ap@gmail.com>
2020-05-17 11:12:35 +0200

    enable navigating to module directory in search everywhere

--- src/main/java/at/wtioit/intellij/plugins/odoo/modules/search/OdooModuleSEContributor.java
+++ src/main/java/at/wtioit/intellij/plugins/odoo/modules/search/OdooModuleSEContributor.java
@@ -3,9 +3,11 @@ package at.wtioit.intellij.plugins.odoo.modules.search;
 import at.wtioit.intellij.plugins.odoo.modules.OdooModule;
 import at.wtioit.intellij.plugins.odoo.modules.OdooModuleService;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributor;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -42,16 +44,19 @@ public class OdooModuleSEContributor implements SearchEverywhereContributor<Odoo
     @Override
     public void fetchElements(@NotNull String pattern, @NotNull ProgressIndicator progressIndicator, @NotNull Processor<? super OdooModule> consumer) {
         OdooModuleService moduleService = ServiceManager.getService(project, OdooModuleService.class);
-        for (OdooModule module : moduleService.getModules()) {
-            if (module != null && module.getName().startsWith(pattern)) {
-                consumer.process(new OdooModulePsiElement(module, project));
+        ApplicationManager.getApplication().runReadAction(() -> {
+            for (OdooModule module : moduleService.getModules()) {
+                if (module != null && module.getName().startsWith(pattern)) {
+                    consumer.process(new OdooModulePsiElement(module, project));
+                }
             }
-        }
+        });
     }
 
     @Override
     public boolean processSelectedItem(@NotNull OdooModule selected, int modifiers, @NotNull String searchText) {
-        return false;
+        ((PsiDirectory) selected.getDirectory()).navigate(true);
+        return true;
     }
 
     @Override
