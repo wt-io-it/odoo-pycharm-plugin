aeecc428cb7ab7c49e2c976ff6df6555ab5cd161
Andreas Perhab <andreas.perhab@wt-io-it.at>
2020-09-29 09:33:27 +0200

    run model inheritance test assertions within a project

--- src/test/java/at/wtioit/intellij/plugins/odoo/models/TestModelServiceImpl.java
+++ src/test/java/at/wtioit/intellij/plugins/odoo/models/TestModelServiceImpl.java
@@ -1,6 +1,7 @@
 package at.wtioit.intellij.plugins.odoo.models;
 
 import at.wtioit.intellij.plugins.odoo.BaseOdooPluginTest;
+import at.wtioit.intellij.plugins.odoo.WithinProject;
 import at.wtioit.intellij.plugins.odoo.modules.OdooModule;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.psi.PsiElement;
@@ -24,10 +25,11 @@ public class TestModelServiceImpl extends BaseOdooPluginTest {
     public void testModelInheritance() {
         OdooModelService modelService = ServiceManager.getService(getProject(), OdooModelService.class);
         OdooModel inherited = modelService.getModel("inherited");
-
-        assertNotNull(inherited.getBaseModule().getDirectory());
-        assertEquals("Base Model should be addon3", "PsiDirectory:/src/odoo/addons/addon3", inherited.getBaseModule().getDirectory().toString());
-        assertContainsElements(inherited.getModules().stream().map(OdooModule::getName).collect(Collectors.toList()), "addon3", "addon1");
+        WithinProject.run(getProject(), () -> {
+            assertNotNull(inherited.getBaseModule().getDirectory());
+            assertEquals("Base Model should be addon3", "PsiDirectory:/src/odoo/addons/addon3", inherited.getBaseModule().getDirectory().toString());
+            assertContainsElements(inherited.getModules().stream().map(OdooModule::getName).collect(Collectors.toList()), "addon3", "addon1");
+        });
     }
 
     public void testModelNames() {
