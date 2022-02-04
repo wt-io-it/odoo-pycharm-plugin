669efca3a7c1a65bb36de3566cc2383d66d136ec
bigbear3001 <bigbear.ap@gmail.com>
2022-02-04 17:59:58 +0100

    fix ImportCompletionTest to account for completions with multiple results

--- src/test/java/at/wtioit/intellij/plugins/odoo/ImportCompletionTest.java
+++ src/test/java/at/wtioit/intellij/plugins/odoo/ImportCompletionTest.java
@@ -1,28 +1,63 @@
 package at.wtioit.intellij.plugins.odoo;
 
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.rt.execution.junit.FileComparisonFailure;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class ImportCompletionTest extends BaseOdooPluginTest {
 
-
     public void testFromOdooAddonsImport() {
-        doTest();
+        // this completion fails because there is two completion contributors suggesting odoo.addons.autocomplete
+        // in the normal auto-completion the user would decide for one of them but in the tests it just is empty
+        LookupElement[] result = doTest(FileComparisonFailure.class);
+        String lookupStrings = Arrays.stream(result)
+                .filter((b) -> b.getUserDataString().contains("=at.wtioit.intellij.plugins.odoo.OdooAddonsCompletionContributor@"))
+                .map(LookupElement::getLookupString)
+                .findAny()
+                .orElseThrow(() -> new AssertionError("Expected completion contribution from at.wtioit.intellij.plugins.odoo.OdooAddonsCompletionContributor"));
+        assertEquals("autocomplete", lookupStrings);
     }
 
     public void testFromOdooAddonsImportOtherDirectory() {
         doTest();
     }
 
-    // TODO we probably need to use the ImportResolver to (@see PyCharmInitializer)
-    /*public void testFromOdooAddonsAddon1ImportModels() {
-        doTest();
-    }*/
+    public void testFromOdooAddonsAddon1ImportModels() {
+        // this completion fails because there is two completion contributors suggesting __manifest__ and models
+        // in the normal auto-completion the user would decide for one of them but in the tests it just is empty
+        LookupElement[] result = doTest(FileComparisonFailure.class);
+        String lookupStrings = Arrays.stream(result)
+                .map(LookupElement::getLookupString)
+                .sorted()
+                .collect(Collectors.joining(","));
+        assertEquals("__manifest__,models", lookupStrings);
+    }
 
-    // TODO we probably need to use the ImportResolver to (@see PyCharmInitializer)
-    /*public void testFromOdooAddonsMyOtherAddonImportModels() {
-        doTest();
-    }*/
+    public void testFromOdooAddonsMyOtherAddonImportModels() {
+        // this completion fails because there is two completion contributors suggesting __manifest__ and models
+        // in the normal auto-completion the user would decide for one of them but in the tests it just is empty
+        LookupElement[] result = doTest(FileComparisonFailure.class);
+        String lookupStrings = Arrays.stream(result)
+                .map(LookupElement::getLookupString)
+                .sorted()
+                .collect(Collectors.joining(","));
+        assertEquals("__manifest__,models", lookupStrings);
+    }
 
     public void testFromOdooAddonsNoDependencies() {
-        doTest();
+        // this completion fails because there is two completion contributors suggesting odoo.addons.no_dependencies
+        // in the normal auto-completion the user would decide for one of them but in the tests it just is empty
+        LookupElement[] result = doTest(FileComparisonFailure.class);
+        String lookupStrings = Arrays.stream(result)
+                .filter((b) -> b.getUserDataString().contains("=at.wtioit.intellij.plugins.odoo.OdooAddonsCompletionContributor@"))
+                .map(LookupElement::getLookupString)
+                .findAny()
+                .orElseThrow(() -> new AssertionError("Expected completion contribution from at.wtioit.intellij.plugins.odoo.OdooAddonsCompletionContributor"));
+        assertEquals("no_dependencies", lookupStrings);
     }
 
     public void testFromOdooAddonsMyOtherAddon() {
@@ -30,9 +65,20 @@ public class ImportCompletionTest extends BaseOdooPluginTest {
     }
 
     private void doTest() {
+        doTest(null);
+    }
+
+    private <T> LookupElement[] doTest(Class<T> acceptableFailure) {
         myFixture.configureByFile("completion/import/" + getTestName(true) + ".py");
-        myFixture.completeBasic();
-        myFixture.checkResultByFile("completion/import/" + getTestName(true) + ".after.py");
+        LookupElement[] result = myFixture.completeBasic();
+        try {
+            myFixture.checkResultByFile("completion/import/" + getTestName(true) + ".after.py");
+        } catch (Throwable t) {
+            if (acceptableFailure == null || !acceptableFailure.isAssignableFrom(t.getClass())) {
+                throw t;
+            }
+        }
+        return result;
     }
 
 
