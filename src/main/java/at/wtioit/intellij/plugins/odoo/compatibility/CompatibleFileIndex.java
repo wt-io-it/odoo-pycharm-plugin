620508fc55b16691df8772f3239f21fd4f06be56
Andreas Perhab <andreas.perhab@wt-io-it.at>
2022-09-14 15:12:59 +0200

    workaround for intellij indexing bug
    https://github.com/wt-io-it/odoo-pycharm-plugin/issues/33

--- src/main/java/at/wtioit/intellij/plugins/odoo/compatibility/CompatibleFileIndex.java
+++ src/main/java/at/wtioit/intellij/plugins/odoo/compatibility/CompatibleFileIndex.java
@@ -1,6 +1,7 @@
 package at.wtioit.intellij.plugins.odoo.compatibility;
 
 import at.wtioit.intellij.plugins.odoo.WithinProject;
+import at.wtioit.intellij.plugins.odoo.index.IndexWatcher;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -11,6 +12,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class CompatibleFileIndex {
@@ -18,6 +20,9 @@ public class CompatibleFileIndex {
      * Depending on availability in the current JVM runs FilenameIndex#getVirtualFilesByName(String, GlobalSearchScope)
      * for newer versions (>2021.2) or runs FilenameIndex#getFilesByName(Project, String, GlobalSearchScope) and
      * converts the result to a collection of VirtualFiles
+     * -
+     * Holds a workaround for https://youtrack.jetbrains.com/issue/IDEA-291382/Assertion-failed-at-VirtualDirectoryImpldoFindChildById
+     * where getVirtualFilesByName might provoke an error when indexing is still to be done
      * @see FilenameIndex#getVirtualFilesByName(String, GlobalSearchScope)
      * @see FilenameIndex#getFilesByName(Project, String, GlobalSearchScope)
      * @param name - name to search
@@ -25,6 +30,18 @@ public class CompatibleFileIndex {
      * @return collection of virtual files matching the name
      */
     public static Collection<VirtualFile> getVirtualFilesByName(String name, GlobalSearchScope scope) {
+        Project project = WithinProject.INSTANCE.get();
+        if (!IndexWatcher.isFullyIndexed(project)) {
+            // When project is not fully indexed calling getVirtualFilesByName or getFilesByName might produce an error
+            // with VirtualDirectoryImpl#doFindChildById so until we can be "sure" that our call doesn't provoke anybody
+            // to slip an error into our plugin error reporter.
+            // https://github.com/wt-io-it/odoo-pycharm-plugin/issues/33
+            // https://youtrack.jetbrains.com/issue/IDEA-291382/Assertion-failed-at-VirtualDirectoryImpldoFindChildById
+            // https://youtrack.jetbrains.com/issue/IDEA-289822
+            // https://github.com/jonathanlermitage/intellij-extra-icons-plugin/issues/106
+            return Collections.emptyList();
+        }
+
         try {
             Method getVirtualFilesByName = FilenameIndex.class.getDeclaredMethod("getVirtualFilesByName", String.class, GlobalSearchScope.class);
             return (Collection<VirtualFile>) getVirtualFilesByName.invoke(FilenameIndex.class, name, scope);
@@ -35,7 +52,7 @@ public class CompatibleFileIndex {
         }
         try {
             Method getFilesByName = FilenameIndex.class.getDeclaredMethod("getFilesByName", Project.class, String.class, GlobalSearchScope.class);
-            PsiFile[] files = (PsiFile[]) getFilesByName.invoke(FilenameIndex.class, WithinProject.INSTANCE.get(), name, scope);
+            PsiFile[] files = (PsiFile[]) getFilesByName.invoke(FilenameIndex.class, project, name, scope);
             return Arrays.stream(files).map(PsiFile::getVirtualFile).collect(Collectors.toList());
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e2) {
             throw new RuntimeException(e2);
--- src/main/java/at/wtioit/intellij/plugins/odoo/index/IndexWatcher.java
+++ src/main/java/at/wtioit/intellij/plugins/odoo/index/IndexWatcher.java
@@ -1,5 +1,10 @@
 package at.wtioit.intellij.plugins.odoo.index;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.util.indexing.UnindexedFilesUpdater;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.function.Supplier;
 
 public class IndexWatcher extends ThreadLocal<IndexWatcher.IndexState> {
@@ -15,6 +20,25 @@ public class IndexWatcher extends ThreadLocal<IndexWatcher.IndexState> {
         } finally {
             INSTANCE.get().insideIndex = false;
         }
+    }
+
+    public static boolean isFullyIndexed(Project project) {
+        boolean fullyScanned = true;
+        try {
+            Method isProjectContentFullyScanned = UnindexedFilesUpdater.class.getDeclaredMethod("isProjectContentFullyScanned", Project.class);
+            fullyScanned = (boolean) isProjectContentFullyScanned.invoke(UnindexedFilesUpdater.class, project);
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            // we ignore exceptions here because isProjectContentFullyScanned is marked for deletion with 2023.1
+            // we still need it as an indicator, because when switching projects we might be called right aways before
+            // the "official" isIndexUpdateInProgress is returning true
+        }
+        if (!fullyScanned) {
+            return false;
+        }
+        if (UnindexedFilesUpdater.isIndexUpdateInProgress(project)) {
+            return false;
+        }
+        return true;
     }
 
     static class IndexState {
