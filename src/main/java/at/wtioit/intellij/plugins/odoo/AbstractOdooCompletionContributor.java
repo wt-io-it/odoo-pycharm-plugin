c26ab5064e114cbae5045b491de8f4c6826ac6e7
bigbear3001 <bigbear.ap@gmail.com>
2022-10-11 13:28:56 +0200

    enable searching with prefix matcher

--- src/main/java/at/wtioit/intellij/plugins/odoo/AbstractOdooCompletionContributor.java
+++ src/main/java/at/wtioit/intellij/plugins/odoo/AbstractOdooCompletionContributor.java
@@ -9,6 +9,7 @@ import at.wtioit.intellij.plugins.odoo.records.OdooRecordService;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.PrefixMatcher;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
@@ -25,10 +26,16 @@ abstract class AbstractOdooCompletionContributor extends CompletionContributor {
         OdooModuleService moduleService = ServiceManager.getService(parameters.getOriginalFile().getProject(), OdooModuleService.class);
         PsiDirectory odooDirectory = moduleService.getOdooDirectory();
         if (odooDirectory != null) {
+            PrefixMatcher matcher =  result.getPrefixMatcher();
+
             for (OdooModule module : moduleService.getModules()) {
+                if (result.isStopped()) {
+                    // cancel further suggestions
+                    return;
+                }
                 String moduleName = module.getName();
                 String pythonModuleImportPath = "odoo.addons." + moduleName;
-                if (moduleName.startsWith(value) && !pythonModuleImportPath.equals(getModulePythonImportPath(odooDirectory, module))) {
+                if ((moduleName.startsWith(value) || matcher.prefixMatches(moduleName)) && !pythonModuleImportPath.equals(getModulePythonImportPath(odooDirectory, module))) {
                     PsiElement directory = module.getDirectory();
                     if (directory != null) {
                         LookupElementBuilder element = LookupElementBuilder
@@ -71,9 +78,14 @@ abstract class AbstractOdooCompletionContributor extends CompletionContributor {
     void suggestModelName(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result, String value) {
         Project project = parameters.getOriginalFile().getProject();
         OdooModelService modelService = ServiceManager.getService(project, OdooModelService.class);
+        PrefixMatcher matcher =  result.getPrefixMatcher();
 
         for (String modelName : modelService.getModelNames()) {
-            if (modelName != null && modelName.startsWith(value)) {
+            if (result.isStopped()) {
+                // cancel further suggestions
+                return;
+            }
+            if (modelName != null && (modelName.startsWith(value) || matcher.prefixMatches(modelName))) {
                 OdooModule module = modelService.getModel(modelName).getBaseModule();
                 if (module != null) {
                     WithinProject.run(project, () -> {
@@ -93,12 +105,14 @@ abstract class AbstractOdooCompletionContributor extends CompletionContributor {
 
     void suggestRecordXmlId(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result, String value) {
         OdooRecordService recordService = ServiceManager.getService(parameters.getOriginalFile().getProject(), OdooRecordService.class);
+        PrefixMatcher matcher =  result.getPrefixMatcher();
+
         for (String xmlId : recordService.getXmlIds()) {
             if (result.isStopped()) {
                 // cancel further suggestions
                 return;
             }
-            if (xmlId.startsWith(value)) {
+            if (xmlId.startsWith(value) || matcher.prefixMatches(xmlId)) {
                 // TODO if we can know the model (e.g. field, only suggest records with matching model)
                 OdooRecord record = recordService.getRecord(xmlId);
                 WithinProject.run(parameters.getOriginalFile().getProject(), () -> {
